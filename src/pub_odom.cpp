ad3542e47d5b6f67b27f4a3bf5cec911bf2dea1a
niscode <nishimura.s405@gmail.com>
2023-05-23 15:24:56 +0900

    make sure to execute scripts in Expo-Diff type

--- src/pub_odom.cpp
+++ src/pub_odom.cpp
@@ -12,13 +12,12 @@ double vth = 0.0;
 double odom_kv = 1.0;
 double odom_kth = 1.0;
 
-bool receive_flag = false;
+int receive_flag = 0;
 
 void roverOdomCallback(const geometry_msgs::Twist::ConstPtr& rover_odom){
   vx = odom_kv * rover_odom->linear.x;
-  vy = odom_kv * rover_odom->linear.y;
   vth = odom_kth * rover_odom->angular.z;
-  receive_flag = true;
+  receive_flag = 1;
 }
 
 int main(int argc, char** argv){
@@ -26,9 +25,9 @@ int main(int argc, char** argv){
 
   ros::NodeHandle n;  // ... Publisher/Subscriber を作るための「ros::NodeHandle」のインスタンスを用意する（Pythonではrospy)
 
-  ros::Publisher  odom_pub = n.advertise<nav_msgs::Odometry>("odom", 1);  // ... Publiserを作成 テンプレート引数「nav_msgs::Odometry」を与える
+  ros::Publisher  odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);  // ... Publiserを作成 テンプレート引数「nav_msgs::Odometry」を与える
 
-  ros::Subscriber odom_sub = n.subscribe("/rover_odo", 1, roverOdomCallback);  // ... Subscriverを作成 「/rover_odo」という名前でキューのサイズが「100」、メッセージを受信した時の処理が「roverOdomCallBack」という関数になるように登録
+  ros::Subscriber odom_sub = n.subscribe("/rover_odo", 100, roverOdomCallback);  // ... Subscriverを作成 「/rover_odo」という名前でキューのサイズが「100」、メッセージを受信した時の処理が「roverOdomCallBack」という関数になるように登録
 
   tf::TransformBroadcaster odom_broadcaster;
   // tf2_ros::TransformBroadcaster odom_broadcaster;
@@ -47,62 +46,66 @@ int main(int argc, char** argv){
 
   ros::Rate r(20);
   while(n.ok()){
-    if (receive_flag)
-    {
-      current_time = ros::Time::now();
 
-      //compute odometry in a typical way given the velocities of the robot
-      double dt = (current_time - last_time).toSec();
-      // Euler
-      // double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-      // double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-      // Runge–Kutta
-      double delta_x = vx * dt;
-      double delta_y = vy * dt;
-      x += delta_x * std::cos(th + vth * dt / 2) - delta_y * std::sin(th + vth * dt / 2);
-      y += delta_x * std::sin(th + vth * dt / 2) + delta_y * std::cos(th + vth * dt / 2);
-      th += vth * dt;
+    ros::spinOnce();               // check for incoming messages
+    current_time = ros::Time::now();
 
-      //since all odometry is 6DOF we'll need a quaternion created from yaw
-      geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+    //compute odometry in a typical way given the velocities of the robot
+    double dt = (current_time - last_time).toSec();
+    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+    double delta_th = vth * dt;
 
-      //first, we'll publish the transform over tf
-      geometry_msgs::TransformStamped odom_trans;
-      odom_trans.header.stamp = current_time;
-      odom_trans.header.frame_id = "odom";
-      odom_trans.child_frame_id = "base_link";
+    x += delta_x;
+    y += delta_y;
+    th += delta_th;
 
-      odom_trans.transform.translation.x = x;
-      odom_trans.transform.translation.y = y;
-      odom_trans.transform.translation.z = 0.0;
-      odom_trans.transform.rotation = odom_quat;
+    //since all odometry is 6DOF we'll need a quaternion created from yaw
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
-      //send the transform
-      odom_broadcaster.sendTransform(odom_trans);
-
-      //next, we'll publish the odometry message over ROS
-      nav_msgs::Odometry odom;
-      odom.header.stamp = current_time;
-      odom.header.frame_id = "odom";
-  
-      //set the position
-      odom.pose.pose.position.x = x;
-      odom.pose.pose.position.y = y;
-      odom.pose.pose.position.z = 0.0;
-      odom.pose.pose.orientation = odom_quat;
-
-      //set the velocity
-      odom.child_frame_id = "base_link";
-      odom.twist.twist.linear.x = vx;
-      odom.twist.twist.linear.y = vy;
-      odom.twist.twist.angular.z = vth;
-
-      //publish the message
-      odom_pub.publish(odom);
-
-      last_time = current_time;
+    //check null quaternion
+    if(odom_quat.x == 0.0 && odom_quat.y == 0.0 && odom_quat.z == 0.0 && odom_quat.w == 0.0){
+      odom_quat.w = 1.0;
     }
-    r.sleep();
+
+    //first, we'll publish the transform over tf
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.stamp = current_time;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
+
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
+    odom_trans.transform.translation.z = 0.0;
+    odom_trans.transform.rotation = odom_quat;
+
+    //send the transform
+    odom_broadcaster.sendTransform(odom_trans);
+
+    //next, we'll publish the odometry message over ROS
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
+
+    //set the position
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.orientation = odom_quat;
+
+    //set the velocity
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x = vx;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vth;
+
+    //publish the message
+    odom_pub.publish(odom);
+
+
+    last_time = current_time;
     ros::spinOnce();
+    r.sleep();
+
   }
 }
