2acaf6695cc2f1bbb1bda63944de90dd30184948
Arne Hitzmann <arne.hitzmann@gmail.com>
2023-06-01 12:26:56 +0900

    added convenience_server dedicated to offer simple jobs via service call

---
+++ scripts/convenience_server_node.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import math
+import numpy as np
+import rospy
+import actionlib
+from actionlib_msgs.msg import GoalID, GoalStatusArray, GoalStatus
+from std_msgs.msg import Header
+from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from telecoV.msg import RobotStatus
+from telecoV.srv import GoalHeadingService, GoalHeadingServiceResponse
+import tf2_ros
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+REFERENCE_FRAME = 'map'
+ROBOT_FRAME = 'base_link'
+
+
+class ConvenienceServer:
+    def __init__(self) -> None:
+        rospy.init_node("convenience_server_node")
+
+        self._reference_frame = rospy.get_param('/convenience_server/reference_frame', REFERENCE_FRAME)
+        self._robot_frame = rospy.get_param('/convenience_server/robot_frame', ROBOT_FRAME)
+
+        # rospy.loginfo('Waiting for move_base')
+        # self._move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        # self._move_base_client.wait_for_server()
+
+        self._goal_heading_debug_publisher = rospy.Publisher('/convenience_server/debug/goal_heading', PoseStamped, queue_size=10)
+        rospy.Subscriber('/robot_status/status', RobotStatus, self._robot_status_cb, queue_size=1)
+        self._goal_heading_service = rospy.Service('/convenience_server/goal_heading', GoalHeadingService, self._goal_heading_service_cb)
+
+        self._tf_buffer = tf2_ros.Buffer()
+        self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
+
+        self._latest_robot_status = RobotStatus()
+
+        rospy.loginfo('Convenience_Server started')
+
+    def _robot_status_cb(self, msg: RobotStatus) -> None:
+        self._latest_robot_status = msg
+
+    def _goal_heading_service_cb(self, msg: GoalHeadingService) -> GoalHeadingServiceResponse:
+        robot_transform = self._tf_buffer.lookup_transform(self._reference_frame, self._robot_frame, rospy.Time())
+
+        robot_vec = np.array([robot_transform.transform.translation.x,
+                              robot_transform.transform.translation.y])
+        goal_vec = np.array([msg.goal_pose.pose.position.x,
+                             msg.goal_pose.pose.position.y])
+        difference_vec = goal_vec - robot_vec
+
+        absolute_yaw = math.atan2(difference_vec[1], difference_vec[0])
+
+        _, _, robot_yaw = euler_from_quaternion([robot_transform.transform.rotation.x, robot_transform.transform.rotation.y,
+                                                 robot_transform.transform.rotation.z, robot_transform.transform.rotation.w])
+
+        relative_yaw = absolute_yaw - robot_yaw
+        goal_heading_quaternion = Quaternion(*quaternion_from_euler(0.0, 0.0, relative_yaw))
+
+        if self._goal_heading_debug_publisher.get_num_connections() > 0:
+            debug_pose = PoseStamped()
+            debug_pose.header.stamp = rospy.Time.now()
+            debug_pose.header.frame_id = self._robot_frame
+            debug_pose.pose.position = Point()
+            debug_pose.pose.orientation = goal_heading_quaternion
+            self._goal_heading_debug_publisher.publish(debug_pose)
+
+        return GoalHeadingServiceResponse(goal_heading_quaternion)
+
+    def run(self) -> None:
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        cs = ConvenienceServer()
+        cs.run()
+    except rospy.ROSInterruptException:
+        pass
