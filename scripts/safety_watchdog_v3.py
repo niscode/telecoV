45a7de96f00083496506cf7500afbd7a52189eda
Arne Hitzmann <arne.hitzmann@gmail.com>
2023-07-17 10:26:46 +0900

    first working version of cloud based safety_watchdog

---
+++ scripts/safety_watchdog_v3.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import math
+import rospy
+from std_srvs.srv import Empty
+from std_msgs.msg import Float32, Header
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+from actionlib_msgs.msg import GoalID, GoalStatusArray, GoalStatus
+from dynamic_reconfigure.server import Server
+from telecoV.cfg import SafetyWatchdogConfig
+from telecoV.msg import StringArray
+from xmlrpc.client import ServerProxy as XMLServerProxy
+
+DEFAULT_ROBOT_RADIUS = 0.2
+PROXIMITY_THRESHOLD = 0.5
+VOLTAGE_THRESHOLD = 20.5
+
+
+class SafetyWatchdog:
+    def __init__(self) -> None:
+        rospy.init_node("safety_watchdog")
+        self._ros_master = XMLServerProxy(os.environ['ROS_MASTER_URI'])
+        self._rosnode_dynamically_loaded = __import__('rosnode')
+
+        if not self._wait_for_node('/move_base', 10.0):
+            rospy.logerr('move_base node not available')
+            sys.exit(0)
+
+        self._current_status = GoalStatus.SUCCEEDED
+        self._minimum_laser_range = -1.0
+        self._current_retry = 1
+        self._goal_header_id_during_incident = 0
+
+        self._voltage_warning_flag = False
+        self._proximity_threshold = PROXIMITY_THRESHOLD
+        self._current_points_in_perimeter = list()
+        self._dyn_rec_server = Server(SafetyWatchdogConfig, self._reconf_callback)
+
+        self._robot_radius = rospy.get_param('/move_base/global_costmap/robot_radius', DEFAULT_ROBOT_RADIUS)
+        rospy.Subscriber('/merged_cloud', PointCloud2, self._pc2_cb, queue_size=1)
+        rospy.Subscriber('/voltage', Float32, self._voltage_cb, queue_size=1)
+        rospy.Subscriber('/move_base/status', GoalStatusArray, self._move_base_status_cb, queue_size=1)
+        self._laser_scan_debug_pub = rospy.Publisher('/safety_watchdog/laser_scan_debug', PointCloud2, queue_size=1)
+        self._navigation_cancel_publisher = rospy.Publisher('/move_base/cancel', GoalID, queue_size=10)
+        self._robot_status_msg_publisher = rospy.Publisher('/robot_status/console_msg', StringArray, queue_size=10)
+        self._patrol_cancel_service = rospy.ServiceProxy('/patrol/cancel', Empty)
+
+        rospy.loginfo('Safety_Watchdog started')
+
+    def _wait_for_node(self, node_name: str, timeout: float) -> bool:
+        start_time = rospy.Time.now().to_sec()
+        while start_time + timeout >= rospy.Time.now().to_sec():
+            try:
+                if node_name in self._rosnode_dynamically_loaded.get_node_names():
+                    return True
+            except self._rosnode_dynamically_loaded.ROSNodeIOException:
+                return False
+        return False
+
+    def _reconf_callback(self, config, level):
+        self._proximity_threshold = float(config.proximity_threshold)
+        return config
+
+    def _move_base_status_cb(self, msg: GoalStatusArray) -> None:
+        if msg.status_list:
+            self._current_status = msg.status_list[-1].status
+
+    def _voltage_cb(self, msg: Float32) -> None:
+        if self._voltage_warning_flag:
+            return
+
+        if float(msg.data) < VOLTAGE_THRESHOLD:
+            rospy.logwarn('Voltage approaching minimum. Consider end of operation.')
+            self._voltage_warning_flag = True
+
+    def _is_in_perimeter(self, point):
+        if not -self._robot_radius <= point.y <= self._robot_radius:
+            return False
+        if not self._robot_radius < point.x <= self._proximity_threshold + self._robot_radius:
+            return False
+        return True
+
+    def _pc2_cb(self, msg: PointCloud2) -> None:
+        header = Header(stamp=rospy.Time.now(), frame_id='laser')
+        point_list = pc2.read_points_list(msg, skip_nans=True, field_names=("x", "y", "z"))
+
+        # Remove robot reflections
+        point_list = [p for p in point_list if math.sqrt(p.x ** 2 + p.y ** 2) >= self._robot_radius]
+
+        # Remove points outside of perimeter
+        point_list = [p for p in point_list if self._is_in_perimeter(p)]
+
+        point_fields = [PointField('x', 0, PointField.FLOAT32, 1), PointField('y', 4, PointField.FLOAT32, 1), PointField('z', 8, PointField.FLOAT32, 1)]
+        debug_msg = pc2.create_cloud(header, fields=point_fields, points=point_list)
+        self._current_points_in_perimeter = point_list
+        self._laser_scan_debug_pub.publish(debug_msg)
+
+    def _warning_message(self):
+        rospy.logwarn(f'Obstacle appeared in front of robot... Canceling navigation...')
+        console_msg = StringArray()
+        console_msg.data.append(f'物体は{self._proximity_threshold}メートルより近くに見えた。航行は中止された。')
+        console_msg.data.append(f'周囲を確認し、新しいナビゲーション・ゴールをリクエストしてください。')
+        self._robot_status_msg_publisher.publish(console_msg)
+
+    def _cancel_navigation(self):
+        self._patrol_cancel_service()
+        empty_goal = GoalID()
+        for i in range(5):
+            self._navigation_cancel_publisher.publish(empty_goal)
+
+    def _abort_conditions_met(self):
+        return self._current_status == GoalStatus.ACTIVE and len(self._current_points_in_perimeter) > 0
+
+    def run(self) -> None:
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if self._abort_conditions_met():
+                self._cancel_navigation()
+                self._warning_message()
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        sw = SafetyWatchdog()
+        sw.run()
+    except rospy.ROSInterruptException:
+        pass
