76a6a13e7afc3c0c098a38c433ccc33c8e260aa3
niscode <nishimura.s405@gmail.com>
2024-02-05 17:08:44 +0900

    add 2F map, bring up with auto login from takasaki_capf

--- scripts/bringup_telecoV.sh
+++ scripts/bringup_telecoV.sh
@@ -8,7 +8,7 @@ tmux split-window -v
 tmux split-window -h
 tmux split-window -v
 tmux split-window -v
-tmux send-keys -t $session:$base_window_name.0 "roslaunch telecoV navigation_teb_diff.launch rover_type:=mega3 start_rviz:=false" Enter
+tmux send-keys -t $session:$base_window_name.0 "roslaunch telecoV navigation_teb_diff.launch rover_type:=mega3 start_rviz:=false map_file:=atr_2f.yaml" Enter
 sleep 5
 tmux send-keys -t $session:$base_window_name.1 "roslaunch telecoV waypoint_server.launch cli:=$use_waypoint_server_cli" Enter
 tmux send-keys -t $session:$base_window_name.2 "roslaunch telecoV patrol.launch" Enter
@@ -17,7 +17,9 @@ tmux send-keys -t $session:$base_window_name.4 "roslaunch telecoV convenience_se
 tmux new-window -n $capf_window_name;
 tmux split-window -v
 tmux split-window -v
+tmux split-window -h
 tmux send-keys -t $session:$capf_window_name.0 "roslaunch takasaki_capf teleco_basic.launch stress_test:=false" Enter
 tmux send-keys -t $session:$capf_window_name.1 "rosrun telecoV safety_watchdog.py" Enter
 tmux send-keys -t $session:$capf_window_name.2 "roscd takasaki_capf ; rosrun takasaki_capf lipsync.py" Enter
+tmux send-keys -t $session:$capf_window_name.3 "roscd takasaki_capf ; rosrun takasaki_capf capf_auto_login.py 0" Enter
 tmux attach -t "$session:$base_window_name"
